64c9526fda6bb36f0ec51da481d2d96eda2f89fc
Dalton <thisisnotdalton@gmail.com>
2015-04-22 22:53:08 -0400

    Camera Changes

--- Assets/_Scripts/Player.cs
+++ Assets/_Scripts/Player.cs
@@ -32,22 +32,32 @@ public class Player : MonoBehaviour {
 	public void Awake(){
 		smallHelp = GameObject.Find ("Small Help Text").gameObject;
 		fullHelp = GameObject.Find ("Full Help Text").gameObject;
-		fullHelp.SetActive (false);
+		smallHelp.SetActive (false);
 	}
 	
 	void Update () {
-		//update camera rotation
-		float rotateSpeedFactor = Vector3.Distance (centerOfScreen, Input.mousePosition) / minRotateDistance;
-		if (rotateSpeedFactor>1) {
-			lookVector = (new Vector3 (-Input.mousePosition.y + Screen.height / 2, Input.mousePosition.x - Screen.width / 2, 0)).normalized * Time.deltaTime* Mathf.Pow(rotateSpeedFactor,2) * cameraSpeed;
-		} else {
-			lookVector=Vector3.zero;
-		}
-
+		//handle pausing and resuming the simulation
 		if (Input.GetKeyDown (KeyCode.H)) {
 			fullHelp.SetActive(!fullHelp.activeSelf);
 			smallHelp.SetActive(!smallHelp.activeSelf);
 		}
+
+		//auto pause simulation if the mouse is off the screen
+		if(Mathf.Abs((centerOfScreen-Input.mousePosition).x)*1.875>Screen.width||Mathf.Abs((centerOfScreen-Input.mousePosition).y)*1.875>Screen.height){
+			smallHelp.SetActive (false);
+			fullHelp.SetActive (true);
+		}
+
+		//if the help text is not visible
+		if(smallHelp.activeSelf){
+		//update camera rotation
+		float rotateSpeedFactor = Vector3.Distance (centerOfScreen, Input.mousePosition) / minRotateDistance;
+		if (rotateSpeedFactor>1) {
+				lookVector = (new Vector3 (2*(-Input.mousePosition.y/Screen.height + Screen.height / 2), 2*(Input.mousePosition.x - Screen.width / 2)/Screen.width, 0)).normalized * Time.deltaTime* Mathf.Pow(rotateSpeedFactor,2) * cameraSpeed;
+		} else {
+			lookVector=Vector3.zero;
+		}
+
 
 		//playerCam.rotation = Quaternion.Euler (playerCam.rotation.eulerAngles + lookVector);
 		playerCam.rotation *= Quaternion.Euler (lookVector);
@@ -62,5 +72,6 @@ public class Player : MonoBehaviour {
 		//move player in direction transform is facing
 		moveVector = (Input.GetAxis ("Horizontal")*transform.right + Input.GetAxis ("Vertical")*transform.forward+Vector3.up*(Input.GetAxis("Jump"))) * moveSpeed *(Input.GetAxis("Sprint")!=0?3:1)* Time.deltaTime;
 		charCon.Move (moveVector);
+		}
 	}
 }
